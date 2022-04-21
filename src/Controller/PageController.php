38fc369c90e171abced8da2dec19d3f76763651d
codeuronline <j.kasperski@codeur.online>
2022-04-21 11:36:09 +0200

    update

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,19 +1,10 @@
-<?php
-// src/Controller/LuckyController.php
+<?php 
 namespace App\Controller;
-
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
-{
-    /** 
-     * 
-    */
-       
-    public function number(): Response
-    {
-        $number = random_int(0, 100);
 
+class PageController{
+    public function index() : Response {
         return new Response(
             '<!DOCTYPE html>
             <html lang="en">
@@ -22,10 +13,23 @@ class LuckyController
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Page '.$number.'</title>
+                <title>Page d\'accueil</title>
             </head>
             <body>
-            <center>
-            <h1><a href="../page/">le Lien</a</h1></center></body></html>');
-    }
+            <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Acccueil</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Retour</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+            </body></html>');
+            }
 }
